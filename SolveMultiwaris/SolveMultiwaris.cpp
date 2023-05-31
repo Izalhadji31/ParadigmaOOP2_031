b5229dd2808b4f406c429a0ca76c1129a2fb6180
Izalhadji31 <ryzalhadji@gmail.com>
2023-05-31 17:23:22 +0700

    Membuat method class pekerja : virtual public orang

--- SolveMultiwaris/SolveMultiwaris.cpp
+++ SolveMultiwaris/SolveMultiwaris.cpp
@@ -12,6 +12,14 @@ public:
     }
 };
 
+class pekerja : virtual public orang {
+public:
+    pekerja(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pekerja dibuat\n" << endl;
+    }
+};
 
 int main()
 {
