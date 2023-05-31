7686238a0bf58a65b6937a1b8addfd7dc4eb6cbf
Izalhadji31 <ryzalhadji@gmail.com>
2023-05-31 17:49:07 +0700

    membuat method class orang

--- Program Multiwaris/Program Multiwaris.cpp
+++ Program Multiwaris/Program Multiwaris.cpp
@@ -1,1 +1,15 @@
 #include <iostream>
+using namespace std;
+
+class orang {
+public:
+	string nama;
+
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "Orang dibuat\n" << endl;
+	}
+	~orang() {
+		cout << "Orang dihapus\n" << endl;
+	}
+};
